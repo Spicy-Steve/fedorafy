b1ff17cc9a4d9e6754403e497851e1b18a16cc66
Spicy-Steve <crazygamerdude26@gmail.com>
2025-11-08 10:35:48 +1300

    Added NVIDIA unattended & updated README.md

--- README.md
+++ README.md
@@ -2,4 +2,37 @@
 Post installation script for fedora
 
 #### Start the post-installation setup by running the following command
+
 `wget https://raw.githubusercontent.com/Spicy-Steve/fedorafy/main/fedorafy.sh && sudo sh fedorafy.sh`
+
+<br>
+
+## For unattended installs, use one of the following
+
+### AMD
+
+`wget https://raw.githubusercontent.com/Spicy-Steve/fedorafy/main/unattended/amd/fedorafy-unattended-amd.sh && sudo sh fedorafy.sh`
+
+##### GAMING ADDITIONS
+
+`wget https://raw.githubusercontent.com/Spicy-Steve/fedorafy/main/unattended/amd/fedorafy-unattended-amd-gaming.sh && sudo sh fedorafy.sh`
+
+<br>
+
+### INTEL
+
+`wget https://raw.githubusercontent.com/Spicy-Steve/fedorafy/main/unattended/intel/fedorafy-unattended-intel.sh && sudo sh fedorafy.sh`
+
+##### GAMING ADDITIONS
+
+`wget https://raw.githubusercontent.com/Spicy-Steve/fedorafy/main/unattended/intel/fedorafy-unattended-intel-gaming.sh && sudo sh fedorafy.sh`
+
+<br>
+
+### NVIDIA
+
+`wget https://raw.githubusercontent.com/Spicy-Steve/fedorafy/main/unattended/nvidia/fedorafy-unattended-nvidia.sh && sudo sh fedorafy.sh`
+
+##### GAMING ADDITIONS
+
+`wget https://raw.githubusercontent.com/Spicy-Steve/fedorafy/main/unattended/nvidia/fedorafy-unattended-nvidia-gaming.sh && sudo sh fedorafy.sh`
---
+++ unattended/nvidia/fedorafy-unattended-nvidia.sh
@@ -0,0 +1,55 @@
+# === Detect root privilages ===
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run this script with sudo or as root user instead of $USER"
+    exit 1
+fi
+
+# === Info ===
+echo "Hope you know what you're doing, good luck!"
+
+# === Detect distro ===
+if [ -f /etc/fedora-release ]; then
+    echo "Fedora detected!"
+    echo "Starting post-install setup..."
+else
+    echo "You are not running Fedora! Stopping script..."
+    exit 1
+fi
+
+# === Prevent usage if dnf is not found ===
+if ! command -v dnf &> /dev/null; then
+    echo "Something has gone terribly wrong, dnf was not found!"
+    echo "Try to remedy this by installing dnf (if this is Fedora, of course) or by reinstalling Fedora from https://fedoraproject.org (legitimate site)"
+    exit 1
+fi
+
+# === Set DNF defaultyes to "Y" ===
+echo "Setting DNF to assume 'yes' for all prompts..."
+echo "defaultyes=True" >> /etc/dnf/dnf.conf
+
+# === Add RPM Fusion media repo ===
+echo "Adding required repositories..."
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# === Update the system ===
+echo "Making sure the system is up to date..."
+dnf update -y
+
+# === Swap codecs ===
+echo "Installing full media codec support..."
+dnf swap ffmpeg-free ffmpeg --allowerasing -y
+
+# === Install GPU acceleration packages ===
+echo "Installing NVIDIA driver and GPU accelerated media packages..."
+dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda libva-nvidia-driver
+
+# === Install flatpak ===
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y com.github.tchx84.Flatseal org.localsend.localsend_app com.dec05eba.gpu_screen_recorder
+
+# === Self deletion after everything ===
+echo "Cleaning up..."
+trap 'rm -f -- "$0"' EXIT
+
+echo "=== Setup Complete! ==="
+reboot now
